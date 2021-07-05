a6b12ac6c991addb7d283947110f403453ad24c5
Carlos Pliego <20203tn129@utez.edu.mx>
2021-07-05 10:49:59 -0500

    subida de cambios

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("---------------------------------");
+
+function guardarNota(){
+    var notaNueva = document.getElementById("nota_Nueva").value;
+    document.getElementById("nota_Nueva").value = " ";
+    console.log(notaNueva);
+};
+
+
+
+
